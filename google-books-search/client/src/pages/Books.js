7b106ab1095097953cc2db22c0395cd32ac69866
Ben Shurts <bshurts93@gmail.com>
2019-06-12 12:39:03 -0500

    Server setup for routes, middleware and PORT environment options

--- google-books-search/client/src/pages/Books.js
+++ google-books-search/client/src/pages/Books.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import GoogleBooksAPI from "../utils/GoogleBooksAPI";
 import DeleteBtn from "../components/DeleteBtn";
+import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
@@ -9,4 +10,16 @@ class Books extends Component {
   state = {
     books: []
   };
+
+  componentDidMount() {
+    //   Get books
+  }
+
+  loadAllBooks() {
+    GoogleBooksAPI.getAllBooks().then(res =>
+      this.setState({ books: res.data }).catch(err => console.log(err))
+    );
+  }
 }
+
+export default Books;
