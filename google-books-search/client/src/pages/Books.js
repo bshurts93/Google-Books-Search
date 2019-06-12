68dcc9208d3c5619960ef4f6ed0b23a4a5a1d1ad
Ben Shurts <bshurts93@gmail.com>
2019-06-12 12:31:06 -0500

    Brought in Bootstrap grid elements as components to pass into the Books page

--- google-books-search/client/src/pages/Books.js
+++ google-books-search/client/src/pages/Books.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import GoogleBooksAPI from "../utils/GoogleBooksAPI";
+import DeleteBtn from "../components/DeleteBtn";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Books extends Component {
   state = {
