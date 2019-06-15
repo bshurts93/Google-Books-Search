db4c787821c0db35c738fbe5899184ff557b18e0
Ben Shurts <bshurts93@gmail.com>
2019-06-15 11:18:16 -0500

    mLab credentials added, build ran for deployment

---
+++ client/build/precache-manifest.45af631f3c02c7b7f9937fd8b2acffe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f7a7fd7a2f13508518f",
+    "url": "/static/js/main.2904ace6.chunk.js"
+  },
+  {
+    "revision": "c78f249f9dcde236f214",
+    "url": "/static/js/2.63f3e210.chunk.js"
+  },
+  {
+    "revision": "3f7a7fd7a2f13508518f",
+    "url": "/static/css/main.d9755f47.chunk.css"
+  },
+  {
+    "revision": "882941319c0b378305623437cfabbf93",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://test-user1:test-pass1@ds137827.mlab.com:37827/heroku_nklp0rjb"
+);
 
 // Start the API server
 app.listen(PORT, function() {
